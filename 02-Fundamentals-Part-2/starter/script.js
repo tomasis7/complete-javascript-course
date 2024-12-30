7d0c6ed1eb78b88abaceb9380e49e1003b0b20d4
Tomas Maldonis <tomasis7@gmail.com>
2024-12-30 15:48:18 +0100

    test

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,13 @@
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  birthYeah: 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+  hasDriversLicense: true,
+
+  calcAge: function (birthYeah) {
+    return 2037 - birthYeah;
+  },
+};
+console.log(jonas.calcAge(1991));
